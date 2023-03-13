daf3cadecb2f2bcef3c177deec6980daef492167
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-13 22:45:47 +0530

    added home and history tab

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiOutlineHome, AiOutlineHistory } from "react-icons/ai";
 
 const Sidebar = () => {
 	return (
@@ -6,9 +7,15 @@ const Sidebar = () => {
 			<h1 className='text-5xl text-primary font-extrabold'>WatchLists</h1>
 			<input
 				type='text'
-				className='bg-black px-4 py-2 border border-gray rounded-lg w-4/5 text-xl placeholder:text-gray'
+				className='bg-black px-4 py-2 border border-gray rounded-lg w-full text-xl placeholder:text-gray'
 				placeholder='Search'
 			/>
+			<div className='flex items-center gap-6 p-3 bg-gray text-white text-xl w-full rounded-md'>
+				<AiOutlineHome className='w-7 h-7' /> Home
+			</div>
+			<div className='flex items-center gap-6 p-3 bg-black text-white text-xl w-full rounded-md'>
+				<AiOutlineHistory className='w-7 h-7' /> History
+			</div>
 		</aside>
 	);
 };
