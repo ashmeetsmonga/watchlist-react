9cd90a220aa641bf9bb5ff76d9a4e854521ebd62
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-13 22:50:05 +0530

    added create watchlist button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { AiOutlineHome, AiOutlineHistory } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineHistory, AiOutlineFileAdd } from "react-icons/ai";
 
 const Sidebar = () => {
 	return (
-		<aside className='w-1/4 h-full flex flex-col gap-8 items-center p-8 bg-black outline'>
+		<aside className='w-1/4 h-full flex flex-col gap-10 items-center p-8 bg-black outline'>
 			<h1 className='text-5xl text-primary font-extrabold'>WatchLists</h1>
 			<input
 				type='text'
 				className='bg-black px-4 py-2 border border-gray rounded-lg w-full text-xl placeholder:text-gray'
 				placeholder='Search'
 			/>
-			<div className='flex items-center gap-6 p-3 bg-gray text-white text-xl w-full rounded-md'>
-				<AiOutlineHome className='w-7 h-7' /> Home
+			<div className='flex flex-col w-full gap-6'>
+				<div className='flex items-center gap-6 p-3 bg-gray text-white text-xl w-full rounded-md'>
+					<AiOutlineHome className='w-7 h-7' /> Home
+				</div>
+				<div className='flex items-center gap-6 p-3 bg-black text-white text-xl w-full rounded-md border border-gray'>
+					<AiOutlineHistory className='w-7 h-7' /> History
+				</div>
 			</div>
-			<div className='flex items-center gap-6 p-3 bg-black text-white text-xl w-full rounded-md'>
-				<AiOutlineHistory className='w-7 h-7' /> History
+			<div className='w-full'>
+				<button className='flex justify-center items-center font-bold gap-2 p-3 bg-primary text-black text-xl w-full rounded-md border'>
+					<AiOutlineFileAdd className='w-7 h-7' /> Create Watchlist
+				</button>
 			</div>
 		</aside>
 	);
