a378e70caa10d4c31b95a9711702de93839dc015
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-14 23:47:14 +0530

    popular movies

--- src/components/PopularMovies.jsx
+++ src/components/PopularMovies.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const PopularMovies = () => {
-	return <div>PopularMovies</div>;
+	return (
+		<div className='w-full flex flex-col'>
+			<h2 className='text-3xl text-white'>Popular Movies</h2>
+		</div>
+	);
 };
 
 export default PopularMovies;
