8b0e4e60edd127058c55705faf763cdeb63733c5
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-14 23:45:52 +0530

    popular movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
+import PopularMovies from "../components/PopularMovies";
 import Search from "../components/Search";
 import Sidebar from "../components/Sidebar";
 
@@ -8,6 +9,7 @@ const Home = () => {
 		<div className='flex flex-col gap-8 w-full h-full p-8'>
 			<Hero />
 			<Search />
+			<PopularMovies />
 		</div>
 	);
 };
