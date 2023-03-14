68c827303b9cd8cd652cff02bd39a043dbdcc90f
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-14 23:44:57 +0530

    search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Search = () => {
-	return <div>Search</div>;
+	return (
+		<div className='w-full flex gap-2'>
+			<input
+				type='text'
+				className='w-full p-3 text-lg bg-darkGray text-white border border-gray rounded-lg outline-none'
+			/>
+			<button className='px-3 py-2 text-lg bg-primary text-white rounded-lg'>Search</button>
+		</div>
+	);
 };
 
 export default Search;
