30bdee6b00aba6243d23666f79536e2454452020
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-15 23:02:58 +0530

    added create new list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import CreateNewList from "./components/CreateNewList";
 
 function App() {
 	return (
 		<div className='w-screen h-screen flex bg-darkGray'>
 			<Sidebar />
-			<Home />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/create-new-list' element={<CreateNewList />} />
+			</Routes>
 		</div>
 	);
 }
---
+++ src/components/CreateNewList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateNewList = () => {
+	return <div>CreateNewList</div>;
+};
+
+export default CreateNewList;
