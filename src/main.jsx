104a70eaf30ea363be7673f43ed4df5804f542c8
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-25 19:34:19 +0530

    added react query setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</BrowserRouter>
 );
