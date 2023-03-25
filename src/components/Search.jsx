d8ad65d4565e4277f24e138f179b759ccb702b63
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-25 19:38:36 +0530

    made search responsive

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Search = () => {
 	return (
-		<div className='w-full flex gap-2'>
+		<div className='w-full flex flex-col gap-4 md:flex-row'>
 			<input
 				type='text'
 				placeholder='Search for movies by title'
 				className='w-full p-3 text-lg bg-darkGray text-white border border-gray placeholder:text-gray rounded-lg outline-none'
 			/>
-			<button className='px-3 py-2 text-lg bg-primary text-black rounded-lg'>Search</button>
+			<button className='px-5 py-2 text-lg bg-primary text-black rounded-lg'>Search</button>
 		</div>
 	);
 };
