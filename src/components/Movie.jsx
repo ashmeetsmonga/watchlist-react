d2e7e79e3d90a99bfa038f43eefda40aeeb8926e
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-25 23:09:46 +0530

    movie layout ui

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,9 +3,8 @@ import { getMoviePath } from "../endpoints/endpoints";
 
 const Movie = ({ movie }) => {
 	return (
-		<div className='w-full md:w-[300px] flex flex-col gap-2'>
+		<div className='w-full md:w-[300px] flex flex-col gap-2 cursor-pointer transition-transform hover:scale-105'>
 			<img src={getMoviePath(movie.poster_path)} />
-			<h2 className='text-white text-xl'>{movie.original_title}</h2>
 		</div>
 	);
 };
