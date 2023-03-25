492958fe9e518e19e155f2f1cfbfe5479cc9bffd
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-25 22:50:05 +0530

    added popular movie api

--- src/components/PopularMovies.jsx
+++ src/components/PopularMovies.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 import endpoints from "../endpoints/endpoints";
+import Movie from "./Movie";
 
 const PopularMovies = () => {
+	const { data, isLoading, isError } = useQuery("popular-movies", async () => {
+		const { data } = await axios.get(endpoints.popular);
+		console.log("Popular movies", data);
+		return data;
+	});
+
 	return (
 		<div className='w-full flex flex-col outline outline-white'>
 			<h2 className='text-3xl text-white'>Popular Movies right now</h2>
-			<div className='text-white'>{endpoints.popular}</div>
+			{data && (
+				<div className='w-full p-4 flex flex-col md:flex-row gap-4'>
+					{data.results.map((movie) => (
+						<Movie key={movie.id} movie={movie} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
