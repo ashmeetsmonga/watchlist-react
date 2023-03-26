cfb84af8713c2e6b3dd0f55765d39488f4d6503a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-26 13:08:25 +0530

    added movie details endpoint

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieDetails = ({ movieId }) => {
+	return <div>MovieDetails</div>;
+};
+
+export default MovieDetails;
--- src/endpoints/endpoints.jsx
+++ src/endpoints/endpoints.jsx
@@ -7,3 +7,9 @@ export const endpoints = {
 export const getMoviePath = (url) => {
 	return `https://image.tmdb.org/t/p/w500/${url}`;
 };
+
+export const getMovieDetailsUrl = (id) => {
+	return `https://api.themoviedb.org/3/movie/${id}?api_key=${
+		import.meta.env.VITE_TMDB_API_KEY
+	}&language=en-US`;
+};
