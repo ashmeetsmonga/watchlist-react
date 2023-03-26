efb6204b3a1f27cc9c0e818843bff85408c7a3d4
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-26 13:04:36 +0530

    movie list ui

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { BsPlus } from "react-icons/bs";
 import { getMoviePath } from "../endpoints/endpoints";
 
 const Movie = ({ movie }) => {
 	return (
-		<div className='w-full md:w-[300px] flex flex-col gap-2 cursor-pointer transition-transform hover:scale-105'>
-			<img src={getMoviePath(movie.poster_path)} />
+		<div className='w-full md:w-[300px] relative flex flex-col gap-2 cursor-pointer transition-transform hover:scale-105'>
+			<img src={getMoviePath(movie.poster_path)} alt={movie.original_title} />
+			<button className='absolute top-2 left-2'>
+				<BsPlus className='text-gray w-10 h-10 transition-transform hover:text-white hover:scale-150' />
+			</button>
 		</div>
 	);
 };
