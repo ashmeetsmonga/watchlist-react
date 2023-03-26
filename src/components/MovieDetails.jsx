b886ac7fa574a4b10c02924c2dafc80e2df13e11
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-26 13:09:56 +0530

    added usequery in moviedetails

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { getMovieDetailsUrl } from "../endpoints/endpoints";
 
 const MovieDetails = ({ movieId }) => {
+	const { data, isLoading, isError } = useQuery(movieId, async () => {
+		const url = getMovieDetailsUrl(movieId);
+		const { data } = await axios.get(url);
+		console.log(data);
+		return data;
+	});
+
 	return <div>MovieDetails</div>;
 };
 
