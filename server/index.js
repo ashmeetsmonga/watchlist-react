694ce918b2f02fd0ac0761c7448d5853bde59370
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-04-01 12:18:45 +0530

    not found error added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 require("express-async-errors");
+const errorHandler = require("./middleware/errorHandler");
+const notFound = require("./middleware/notFound");
 require("dotenv").config();
 require("./db/connect");
 const cors = require("cors");
@@ -9,6 +11,10 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
 	console.log("Server started on port:", port);
