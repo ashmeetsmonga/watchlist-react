740883989a84ea148d29d4064393960a8ee3111a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-04-01 23:03:13 +0530

    register ui

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,13 +3,34 @@ import React from "react";
 const Register = () => {
 	return (
 		<div className='w-full flex justify-center items-center outline outline-white'>
-			<form action=''>
-				<label>Name</label>
-				<input type='text' />
-				<label>Email</label>
-				<input type='text' />
-				<label>Password</label>
-				<input type='text' />
+			<form action='' className='flex flex-col gap-4'>
+				<div className='flex flex-col gap-1'>
+					<label className='text-white text-xl'>Name *</label>
+					<input
+						className='px-4 py-2 text-white text-2xl bg-transparent border border-white rounded-md outline-none focus:outline-none'
+						type='text'
+					/>
+				</div>
+				<div className='flex flex-col gap-1'>
+					<label className='text-white text-xl'>Email *</label>
+					<input
+						className='px-4 py-2 text-white text-2xl bg-transparent border border-white rounded-md outline-none focus:outline-none'
+						type='email'
+					/>
+				</div>
+				<div className='flex flex-col gap-1'>
+					<label className='text-white text-xl'>Password *</label>
+					<input
+						className='px-4 py-2 text-white text-2xl bg-transparent border border-white rounded-md outline-none focus:outline-none'
+						type='password'
+					/>
+				</div>
+				<button
+					type='submit'
+					className='flex justify-center items-center font-bold p-3 mt-8 bg-primary text-black text-xl w-full rounded-md border'
+				>
+					Create Profile
+				</button>
 			</form>
 		</div>
 	);
